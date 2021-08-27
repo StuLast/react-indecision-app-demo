a0a708f339831c21601b29fa7cf397e07d8ff659
Stu Last <stuart@theaudioprogrammer.com>
2021-08-27 17:26:55 +0100

    Basic styling completed

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,17 +3,29 @@ import { Option } from "./Option";
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-    {props.options.length === 0 && (
-      <p>Please add add an option to get started</p>
-    )}
-    {props.options.map((option) => (
-      <Option
-        key={option}
-        handleDeleteOption={props.handleDeleteOption}
-        optionText={option}
-      />
-    ))}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button 
+        className="button button--link" 
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+      </button>
+    </div>
+
+      {props.options.length === 0 && (
+        <p className="widget__message">Please add add an option to get started</p>
+      )}
+
+      {props.options.map((option, index) => (
+        <Option
+          key={option}
+          handleDeleteOption={props.handleDeleteOption}
+          optionText={option}
+          count={index + 1}
+        />
+      ))}
+
   </div>
 );
 
